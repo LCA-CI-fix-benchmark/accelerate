67a329fae682bcc7e621de86287c338e1e524c56
jovyan <jovyan@kovrigin-a100-80gb-0>
2024-04-07 20:58:27 +0000

    test_lca_code_editing__code-editing-metrics-x688f9e9d9d296ff9__id_129

--- src/accelerate/utils/modeling.py
+++ src/accelerate/utils/modeling.py
@@ -27,13 +27,14 @@ from typing import Dict, List, Optional, Tuple, Union
 import torch
 import torch.nn as nn
 
+```python
 from ..state import AcceleratorState
 from .constants import SAFE_WEIGHTS_NAME, WEIGHTS_NAME
 from .dataclasses import AutocastKwargs, CustomDtype, DistributedType
-from .imports import is_mps_available, is_npu_available, is_xpu_available, is_peft_available
+from .imports import is_mps_available, is_npu_available, is_peft_available, is_xpu_available
 from .offload import load_offloaded_weight, offload_weight, save_offload_index
 from .tqdm import is_tqdm_available, tqdm
-
+```
 
 if is_npu_available(check_device=False):
     import torch_npu  # noqa: F401
