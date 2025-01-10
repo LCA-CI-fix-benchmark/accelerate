8d7ee8e8c756f4494334071e820eb28e488ed45d
ubuntu <ubuntu@ip-10-0-1-17>
2025-01-10 12:16:42 +0000

    waleko__gemini-pro-1.5_3bee__id_128

--- tests/deepspeed/test_deepspeed.py
+++ tests/deepspeed/test_deepspeed.py
@@ -13,11 +13,6 @@
 # limitations under the License.
 
 import inspect
-import io
-import itertools
-import json
-import os
-import tempfile
 from copy import deepcopy
 from pathlib import Path
 
@@ -29,6 +24,11 @@ from transformers.testing_utils import mockenv_context
 from transformers.trainer_utils import set_seed
 from transformers.utils import is_torch_bf16_available
 
+import io
+import itertools
+import json
+import os
+import tempfile
 import accelerate
 from accelerate.accelerator import Accelerator
 from accelerate.state import AcceleratorState
@@ -36,8 +36,8 @@ from accelerate.test_utils.testing import (
     AccelerateTestCase,
     TempDirTestCase,
     execute_subprocess_async,
-    require_non_cpu,
     require_deepspeed,
+    require_non_cpu,
     require_multi_device,
     slow,
 )
