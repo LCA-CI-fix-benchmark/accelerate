0c37384b451c472492b8852197f1f063b486e41c
ubuntu <ubuntu@ip-10-0-1-7>
2025-01-15 17:20:24 +0000

    waleko__llama-3.1-70b_ff81__id_130

--- src/accelerate/utils/fsdp_utils.py
+++ src/accelerate/utils/fsdp_utils.py
@@ -22,12 +22,12 @@ from .other import extract_model_from_parallel
 from .versions import is_torch_version
 
 
-if is_torch_version(">=", FSDP_PYTORCH_VERSION) and is_torch_distributed_available():
-    import torch.distributed.checkpoint as dist_cp
-    from torch.distributed.checkpoint.default_planner import DefaultLoadPlanner, DefaultSavePlanner
-    from torch.distributed.checkpoint.optimizer import load_sharded_optimizer_state_dict
-    from torch.distributed.fsdp.fully_sharded_data_parallel import FullyShardedDataParallel as FSDP
-    from torch.distributed.fsdp.fully_sharded_data_parallel import StateDictType
+import torch
+from ..logging import get_logger
+from .constants import FSDP_MODEL_NAME, FSDP_PYTORCH_VERSION, OPTIMIZER_NAME
+from .imports import is_torch_distributed_available, is_peft_available
+from .other import extract_model_from_parallel
+from .versions import is_torch_version
 
 
 logger = get_logger(__name__)
