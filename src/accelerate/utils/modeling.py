5e7a85a2c54e7106c695d7e6e7a8ada28bf518eb
ubuntu <ubuntu@ip-10-0-1-83>
2025-01-18 10:45:29 +0000

    waleko__llama-3.1-70b_fa79__id_129

--- src/accelerate/utils/modeling.py
+++ src/accelerate/utils/modeling.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import collections
 import contextlib
 import gc
 import inspect
@@ -21,11 +22,19 @@ import os
 import re
 import shutil
 import tempfile
-from collections import OrderedDict, defaultdict
-from typing import Dict, List, Optional, Tuple, Union
-
 import torch
 import torch.nn as nn
+from typing import Dict, List, Optional, Tuple, Union
+
+from ..state import AcceleratorState
+from .constants import SAFE_WEIGHTS_NAME, WEIGHTS_NAME
+from .dataclasses import AutocastKwargs, CustomDtype, DistributedType
+from .imports import is_mps_available, is_npu_available, is_xpu_available, is_peft_available
+from .offload import load_offloaded_weight, offload_weight, save_offload_index
+from .tqdm import is_tqdm_available, tqdm
+
+from safetensors import safe_open
+from safetensors.torch import load_file as safe_load_file
 
 from ..state import AcceleratorState
 from .constants import SAFE_WEIGHTS_NAME, WEIGHTS_NAME
