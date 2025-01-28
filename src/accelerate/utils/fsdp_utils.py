24bf368adb733bcf4d24d56d1098ce460cdd82df
ubuntu <ubuntu@ip-10-0-1-109>
2025-01-28 19:19:52 +0000

    waleko__claude-3-haiku_ddc6__id_130

--- src/accelerate/utils/fsdp_utils.py
+++ src/accelerate/utils/fsdp_utils.py
@@ -13,14 +13,13 @@
 # limitations under the License.
 import os
 
-import torch
-
 from ..logging import get_logger
 from .constants import FSDP_MODEL_NAME, FSDP_PYTORCH_VERSION, OPTIMIZER_NAME
 from .imports import is_torch_distributed_available, is_peft_available
 from .other import extract_model_from_parallel
 from .versions import is_torch_version
 
+import torch
 
 if is_torch_version(">=", FSDP_PYTORCH_VERSION) and is_torch_distributed_available():
     import torch.distributed.checkpoint as dist_cp
