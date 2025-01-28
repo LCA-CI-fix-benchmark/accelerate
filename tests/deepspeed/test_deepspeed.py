69f5efa5da69595bfa32d71d73846140267512a5
ubuntu <ubuntu@ip-10-0-1-109>
2025-01-28 17:12:56 +0000

    waleko__claude-3-haiku_3f9b__id_128

--- tests/deepspeed/test_deepspeed.py
+++ tests/deepspeed/test_deepspeed.py
@@ -41,6 +41,35 @@ from accelerate.test_utils.testing import (
     require_multi_device,
     slow,
 )
+import inspect
+import io
+import itertools
+import json
+import os
+import tempfile
+from copy import deepcopy
+from pathlib import Path
+
+import torch
+from parameterized import parameterized
+from torch.utils.data import DataLoader
+from transformers import AutoModel, AutoModelForCausalLM, get_scheduler
+from transformers.testing_utils import mockenv_context
+from transformers.trainer_utils import set_seed
+from transformers.utils import is_torch_bf16_available
+
+import accelerate
+from accelerate.accelerator import Accelerator
+from accelerate.state import AcceleratorState
+from accelerate.test_utils.testing import (
+    AccelerateTestCase,
+    TempDirTestCase,
+    execute_subprocess_async,
+    require_non_cpu,
+    require_deepspeed,
+    require_multi_device,
+    slow,
+)
 from accelerate.test_utils.training import RegressionDataset
 from accelerate.utils.dataclasses import DeepSpeedPlugin
 from accelerate.utils.deepspeed import (
