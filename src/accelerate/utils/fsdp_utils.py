7ffb961c627182b415c4ee1dbe8a51284dc4e13c
ubuntu <ubuntu@ip-10-0-1-79>
2025-01-30 15:23:19 +0000

    waleko__claude-3-opus_f4f5__id_130

--- src/accelerate/utils/fsdp_utils.py
+++ src/accelerate/utils/fsdp_utils.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
 
 import torch
 
@@ -19,6 +18,7 @@ from ..logging import get_logger
 from .constants import FSDP_MODEL_NAME, FSDP_PYTORCH_VERSION, OPTIMIZER_NAME
 from .imports import is_torch_distributed_available, is_peft_available
 from .other import extract_model_from_parallel
+import os
 from .versions import is_torch_version
 
 
